e45edaf858b180afe555b8583d6427353d744185
Hyunkuk <scmitar1@gmail.com>
2019-09-05 18:00:10 +0900

    예제 구조 수정

--- app/Application.js
+++ app/Application.js
@@ -20,217 +20,74 @@ Ext.define('Template.Application', {
             maximizable: true,
             items: [
                 {
-                    xtype: 'uxiframe',
-                    listeners: {
-                        afterrender: function () {
-                            var me = this,
-                                pkgNm = 'html2pdf',
-                                win = me.getWin(),
-                                callback = function () {
-                                    Html2pdfUtil.loadLibToIframe({
-                                        window: win,
-                                        template: {
-                                            url: 'resources/html/template2.html',
-                                            config: {
-                                                getNumberValue: function (k) {
-                                                    var v = this.getValue(k);
+                    xclass: 'Html2pdf.view.Preview',
+                    template: {
+                        url: 'resources/html/template2.html',
+                        config: {
+                            getNumberValue: function (k) {
+                                var v = this.getValue(k);
 
-                                                    if (v && Ext.isNumeric(k)) {
-                                                        return Ext.util.Format.number(v, '#,###');
-                                                    } else {
-                                                        return '-';
-                                                    }
-                                                },
-                                                getNumberValueForLoop: function (values, k) {
-                                                    var v = values[k];
+                                if (v && Ext.isNumeric(k)) {
+                                    return Ext.util.Format.number(v, '#,###');
+                                } else {
+                                    return '-';
+                                }
+                            },
+                            getNumberValueForLoop: function (values, k) {
+                                var v = values[k];
 
-                                                    if (v && Ext.isNumeric(v)) {
-                                                        return Ext.util.Format.number(v, '#,###');
-                                                    } else {
-                                                        return '-';
-                                                    }
-                                                },
-                                                getTextValue: function (k) {
-                                                    var v = this.getValue(k);
+                                if (v && Ext.isNumeric(v)) {
+                                    return Ext.util.Format.number(v, '#,###');
+                                } else {
+                                    return '-';
+                                }
+                            },
+                            getTextValue: function (k) {
+                                var v = this.getValue(k);
 
-                                                    if (v) {
-                                                        return '' + v;
-                                                    }
-                                                },
+                                if (v) {
+                                    return '' + v;
+                                }
+                            },
 
-                                                getValue: function (k) {
-                                                    var addr = k.split('.'),
-                                                        values = this.fn.arguments[1],
-                                                        cursor = values, v;
+                            getValue: function (k) {
+                                var addr = k.split('.'),
+                                    values = this.fn.arguments[1],
+                                    cursor = values, v;
 
-                                                    Ext.each(addr, function (str) {
-                                                        v = cursor = cursor && cursor[str];
-                                                    });
+                                Ext.each(addr, function (str) {
+                                    v = cursor = cursor && cursor[str];
+                                });
 
-                                                    return v;
-                                                }
-                                            }
-                                        },
-                                        data: {
-                                            //On-board Handling Charge
-                                            BASIC: {
-                                                DISCHARGING: {
-                                                    QTY: 0,
-                                                    CBM: 0,
-                                                    RT: 0,
-                                                    RATE: 1244,
-                                                    AMOUNT: 123456,
-                                                    RMK: 'TEST'
-                                                },
-                                                LOADING: {
-                                                    QTY: 1,
-                                                    CBM: 1,
-                                                    RT: 1,
-                                                    RATE: 12441,
-                                                    AMOUNT: 1234561,
-                                                    RMK: 'TEST1'
-                                                },
-                                                TS_CHARGING: {
-                                                    QTY: 2,
-                                                    CBM: 2,
-                                                    RT: 2,
-                                                    RATE: 12441,
-                                                    AMOUNT: 1234561,
-                                                    RMK: 'TEST1'
-                                                },
-                                                TS_LOADING: {
-                                                    QTY: 3,
-                                                    CBM: 3,
-                                                    RT: 3,
-                                                    RATE: 12441,
-                                                    AMOUNT: 1234561,
-                                                    RMK: 'TEST1'
-                                                },
-                                                '1TIME': {
-                                                    QTY: 4,
-                                                    CBM: 4,
-                                                    RT: 4,
-                                                    RATE: 12441,
-                                                    AMOUNT: 1234561,
-                                                    RMK: 'TEST1'
-                                                }
-                                            },
-                                            STORAGE_CHARGE: [
-                                                {
-                                                    ITM: 'Over Storage Charge',
-                                                    TYPE: 'Small Size',
-                                                    QTY: 1,
-                                                    OVER_DAY: 1,
-                                                    RATE: 1,
-                                                    AMT: 1000,
-                                                    RMK: 'TEST'
-                                                },
-                                                {
-                                                    ITM: 'Over Storage Charge',
-                                                    TYPE: 'Mild Size',
-                                                    QTY: 2,
-                                                    OVER_DAY: 2,
-                                                    RATE: 2,
-                                                    AMT: 2000,
-                                                    RMK: 'TEST2'
-                                                },
-                                            ]
-                                        },
-                                    })
-                                };
-                            if (Ext.Package && !Ext.Package.isLoaded(pkgNm)) {
-                                Ext.Package.load(pkgNm)
-                                    .then(function () {
-                                        callback();
-                                    });
-                            } else {
-                                callback();
+                                return v;
                             }
                         }
                     },
+                    fileName: 'download.pdf',
+                    loadPreviewData: function (cb) {
+                        Ext.Ajax.request({
+                            url: 'resources/data/sample.json',
+                            success: function (res) {
+                                var data = Ext.decode(res.responseText);
+                                cb(data);
+                            }
+                        });
+                    },
+                    uploadFile: function (file) {
+                        var formData = new FormData();
+                        formData.append('file', file);
+
+                        FileUtil.upload({
+                            url: 'uploadFile',
+                            formData: formData,
+                            callback: function () {
+                                // TODO:
+                            }
+                        })
+                    }
                 }
             ],
-            dockedItems: [
-                {
-                    xtype: 'toolbar',
-                    dock: 'top',
-                    items: [
-                        '->',
-                        {
-                            xtype: 'button',
-                            text: 'Print',
-                            handler: function (btn) {
-                                var panel = btn.up('window').down('uxiframe'),
-                                    win = panel.getWin();
 
-                                win.print();
-                            }
-                        },
-                        {
-                            xtype: 'button',
-                            text: 'PDF',
-                            handler: function (btn) {
-                                var pkgNm = 'html2pdf',
-                                    panel = btn.up('window').down('uxiframe'),
-                                    doc = panel.getDoc(),
-                                    callback = function () {
-                                        Html2pdfUtil.downloadFile({
-                                            element: doc.querySelector('.Content'),
-                                            fileName: 'download.pdf',
-                                            window: panel.getWin()
-                                        });
-                                    };
-
-                                if (Ext.Package && !Ext.Package.isLoaded(pkgNm)) {
-                                    Ext.Package.load(pkgNm)
-                                        .then(function () {
-                                            callback();
-                                        });
-                                } else {
-                                    callback();
-                                }
-                            }
-                        },
-                        {
-                            xtype: 'button',
-                            text: 'E-mail',
-                            handler: function (btn) {
-                                var pkgNm = 'html2pdf',
-                                    panel = btn.up('window').down('uxiframe'),
-                                    doc = panel.getDoc(),
-                                    formData,
-                                    callback = function () {
-                                        Html2pdfUtil.getFile({
-                                            element: doc.querySelector('.Content'),
-                                            fileName: 'download.pdf',
-                                            window: panel.getWin(),
-                                        }).then(function (file) {
-                                            formData = new FormData();
-                                            formData.append('file', file);
-
-                                            FileUtil.upload({
-                                                url: 'uploadFile',
-                                                formData: formData,
-                                                callback: function () {
-                                                    // TODO:
-                                                }
-                                            })
-                                        });
-                                    };
-
-                                if (Ext.Package && !Ext.Package.isLoaded(pkgNm)) {
-                                    Ext.Package.load(pkgNm)
-                                        .then(function () {
-                                            callback();
-                                        });
-                                } else {
-                                    callback();
-                                }
-                            }
-                        }
-                    ]
-                }
-            ]
         }).show();
     },
 
