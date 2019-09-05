0911f9fc5eb46fca18266c3941728e65adaf2b5d
Hyunkuk <scmitar1@gmail.com>
2019-09-05 11:54:06 +0900

    pdf 관련 오류 수정

--- packages/local/html2pdf/src/Util.js
+++ packages/local/html2pdf/src/Util.js
@@ -2,16 +2,18 @@ Ext.define('Html2pdf.Util', {
     alternateClassName: 'Html2pdfUtil',
     singleton: true,
     defaultConfig: {
-        margin: 5,
+        margin: 5
     },
     downloadFile: function (args) {
-        this.loadLib(function () {
-            var inst = html2pdf(),
+        var $me = this,
+            $win = args['window'];
+
+        if ($win && window !== $win) {
+            var inst = $win.html2pdf(),
                 element = args['element'],
                 fileName = args['fileName'],
-                cfg = Ext.apply(this.defaultConfig, {
-                    filename: fileName || 'pdf.pdf',
-                    margin: 5
+                cfg = Ext.apply($me.defaultConfig, {
+                    filename: fileName || 'pdf.pdf'
                 });
 
             inst.set(cfg).from(element).then(function () {
@@ -21,21 +23,42 @@ Ext.define('Html2pdf.Util', {
                     me.save()
                 }, 500);
             });
-        });
+
+        } else {
+            this.loadLib(function () {
+                var inst = html2pdf(),
+                    element = args['element'],
+                    fileName = args['fileName'],
+                    cfg = Ext.apply($me.defaultConfig, {
+                        filename: fileName || 'pdf.pdf'
+                    });
+                inst.set(cfg).from(element).then(function () {
+                    var me = this;
+
+                    setTimeout(function () {
+                        me.save()
+                    }, 500);
+                });
+            });
+        }
+
     },
 
     getFile: function (args) {
-        this.loadLib(function () {
+        var $me = this,
+            $win = args['window'];
+
+        if ($win && $win !== window) {
             var file, ab, idx, uint8Array,
                 element = args['element'],
                 fileName = args['fileName'],
-                cfg = Ext.apply(this.defaultConfig, {
+                cfg = Ext.apply($me.defaultConfig, {
                     filename: fileName || 'pdf.pdf',
                     margin: 5
                 });
 
             return new Promise(function (res) {
-                html2pdf().set(cfg).from(element).then(function () {
+                $win.html2pdf().set(cfg).from(element).then(function () {
                     var me = this;
 
                     setTimeout(function () {
@@ -54,7 +77,39 @@ Ext.define('Html2pdf.Util', {
                     }, 500);
                 });
             });
-        })
+        } else {
+            this.loadLib(function () {
+                var file, ab, idx, uint8Array,
+                    element = args['element'],
+                    fileName = args['fileName'],
+                    cfg = Ext.apply($me.defaultConfig, {
+                        filename: fileName || 'pdf.pdf',
+                        margin: 5
+                    });
+
+                return new Promise(function (res) {
+                    html2pdf().set(cfg).from(element).then(function () {
+                        var me = this;
+
+                        setTimeout(function () {
+                            me.outputPdf().then(function (str) {
+                                ab = new ArrayBuffer(str.length);
+                                uint8Array = new Uint8Array(ab);
+                                idx = str.length;
+                                for (; idx--;) {
+                                    uint8Array[idx] = str.charCodeAt(idx);
+                                }
+                                file = new Blob([ab], {
+                                    type: 'application/pdf'
+                                });
+                                res(file);
+                            })
+                        }, 500);
+                    });
+                });
+            });
+        }
+
     },
 
     loadLib: function (callback) {
@@ -68,5 +123,36 @@ Ext.define('Html2pdf.Util', {
         } else {
             callback();
         }
+    },
+
+    loadLibToIframe: function (cfg) {
+        var script,
+            win = cfg['window'],
+            template = cfg['template'],
+            data = cfg['data'],
+            html = template && template['url'],
+            config = (template && template['config']) || {};
+
+        fetch(html)
+            .then(function (res) {
+                return res.text();
+            })
+            .then(function (html) {
+                win.document.write(new Ext.XTemplate([
+                    html,
+                    config
+                ]).apply(data));
+            })
+            .then(function () {
+                fetch(Ext.getResourcePath('html2pdf') + '/lib/html2pdf.js')
+                    .then(function (res) {
+                        return res.text();
+                    })
+                    .then(function (txt) {
+                        script = win.document.createElement('script');
+                        script.text = txt;
+                        win.document.head.appendChild(script);
+                    });
+            });
     }
 });
