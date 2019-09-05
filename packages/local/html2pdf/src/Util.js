90051b76e9f1f49213d7a7dd08044f9782a5cb8f
Hyunkuk <scmitar1@gmail.com>
2019-09-05 09:54:14 +0900

    Lib 이동

--- packages/local/html2pdf/src/Util.js
+++ packages/local/html2pdf/src/Util.js
@@ -1,14 +1,11 @@
 Ext.define('Html2pdf.Util', {
-    requires: [
-        'Html2pdf.Lib'
-    ],
     alternateClassName: 'Html2pdfUtil',
     singleton: true,
     defaultConfig: {
         margin: 5,
     },
     downloadFile: function (args) {
-        if (window['html2pdf']) {
+        this.loadLib(function () {
             var inst = html2pdf(),
                 element = args['element'],
                 fileName = args['fileName'],
@@ -24,37 +21,52 @@ Ext.define('Html2pdf.Util', {
                     me.save()
                 }, 500);
             });
-        }
+        });
     },
 
     getFile: function (args) {
-        var file, ab, idx, uint8Array,
-            element = args['element'],
-            fileName = args['fileName'],
-            cfg = Ext.apply(this.defaultConfig, {
-                filename: fileName || 'pdf.pdf',
-                margin: 5
-            });
+        this.loadLib(function () {
+            var file, ab, idx, uint8Array,
+                element = args['element'],
+                fileName = args['fileName'],
+                cfg = Ext.apply(this.defaultConfig, {
+                    filename: fileName || 'pdf.pdf',
+                    margin: 5
+                });
 
-        return new Promise(function (res) {
-            html2pdf().set(cfg).from(element).then(function () {
-                var me = this;
+            return new Promise(function (res) {
+                html2pdf().set(cfg).from(element).then(function () {
+                    var me = this;
 
-                setTimeout(function () {
-                    me.outputPdf().then(function (str) {
-                        ab = new ArrayBuffer(str.length);
-                        uint8Array = new Uint8Array(ab);
-                        idx = str.length;
-                        for (; idx--;) {
-                            uint8Array[idx] = str.charCodeAt(idx);
-                        }
-                        file = new Blob([ab], {
-                            type: 'application/pdf'
-                        });
-                        res(file);
-                    })
-                }, 500);
+                    setTimeout(function () {
+                        me.outputPdf().then(function (str) {
+                            ab = new ArrayBuffer(str.length);
+                            uint8Array = new Uint8Array(ab);
+                            idx = str.length;
+                            for (; idx--;) {
+                                uint8Array[idx] = str.charCodeAt(idx);
+                            }
+                            file = new Blob([ab], {
+                                type: 'application/pdf'
+                            });
+                            res(file);
+                        })
+                    }, 500);
+                });
             });
-        });
+        })
+    },
+
+    loadLib: function (callback) {
+        if (!window.html2pdf) {
+            Ext.Loader.loadScript({
+                url: Ext.getResourcePath('html2pdf') + '/lib/html2pdf.js',
+                onLoad: function () {
+                    callback();
+                }
+            });
+        } else {
+            callback();
+        }
     }
 });
